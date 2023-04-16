f02b71577abc4f592df871d1dda8f905afb1a575
Aruminium <b2201680@photon.chitose.ac.jp>
2023-04-16 11:35:15 +0900

    Open Pcap Fileを実装(#2)

--- gopacket-app/openpcap/open_pcap_file.go
+++ gopacket-app/openpcap/open_pcap_file.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"main/conf"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
 )
 
 var (
-	pcapFile string = "test.pcap"
+	pcapFile string = ""
+	handle   *pcap.Handle
+	err      error
 )
+
+func main() {
+	// Read Config
+	config, err := conf.ReadConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	pcapFile = config.PcapFilePath
+
+	// Open file instead of device
+	handle, err = pcap.OpenOffline(pcapFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	// Loop throuph packets in file
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		fmt.Println(packet)
+	}
+}
